630ae751005842a050c61f0fa673ae8b0f872a06
Wirlly Silva <126121325+WirllySilva@users.noreply.github.com>
2023-08-18 15:56:26 -0300

    the field classe has started

--- MineSweeper/src/Main/ControlModel/Field.java
+++ MineSweeper/src/Main/ControlModel/Field.java
@@ -1,5 +1,22 @@
 package Main.ControlModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Field {
+
+    private final int row;
+    private final int column;
+
+    private boolean opened = false;
+    private boolean mined = false;
+    private boolean marked = false;
+
+    private List<Field> neighboringSquares = new ArrayList<>();
+
+    Field(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
     
 }
