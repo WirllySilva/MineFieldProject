512f737cc350c33ca5f15f0863e332978d08767e
Wirlly Silva <126121325+WirllySilva@users.noreply.github.com>
2023-08-28 23:54:31 -0300

    The GameBoard class has been started

--- MineSweeper/src/Main/ControlModel/GameBoard.java
+++ MineSweeper/src/Main/ControlModel/GameBoard.java
@@ -1,5 +1,47 @@
 package Main.ControlModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameBoard {
     
+    private int numberOfrows;
+    private int numberOfcolumns;
+    private int numberOfMines;
+
+    private final List<Field> fields = new ArrayList<>();
+
+
+    public GameBoard(int numberOfrows, int numberOfcolumns, int numberOfMines) {
+        this.numberOfrows = numberOfrows;
+        this.numberOfcolumns = numberOfcolumns;
+        this.numberOfMines = numberOfMines;
+
+        generateField();
+        connectAdjacentsSquares();
+        raffleMines();
+    }
+
+    private void generateField() {
+        for (int row = 0; row < numberOfrows; row++) {
+            for (int column = 0; column < numberOfcolumns; column++) {
+                fields.add(new Field(row, column));
+            }
+        }
+    }
+    
+    private void connectAdjacentsSquares() {
+        for(Field f1: fields) {
+            for(Field f2: fields) {
+                f1.addAdjacentSquare(f2);
+            }
+        }
+    }
+    
+    private void raffleMines() {
+    }
+
+    
+    
+
 }
