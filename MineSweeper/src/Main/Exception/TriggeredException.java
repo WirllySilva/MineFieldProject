e90ceda110007c5cfa62c82893051257a1c9aef2
Wirlly Silva <126121325+WirllySilva@users.noreply.github.com>
2023-08-31 16:14:59 -0300

    Update

--- MineSweeper/src/Main/Exception/TriggeredException.java
+++ MineSweeper/src/Main/Exception/TriggeredException.java
@@ -2,6 +2,6 @@ package Main.Exception;
 
 public class TriggeredException extends RuntimeException {
     
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1;
     
 }
